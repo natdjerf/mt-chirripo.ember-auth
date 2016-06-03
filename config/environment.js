f0e705c79382df7c4b5bbfbe64785f669e2c1d3c
natdjerf <nadjerf@gmail.com>
2016-06-03 15:46:33 -0400

    Update for deployment
    URL & location

--- config/environment.js
+++ config/environment.js
@@ -4,8 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'mt-chirripo.ember-auth',
     environment: environment,
-    baseURL: '/',
-    locationType: 'auto',
+    baseURL: '/chirripo/',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
