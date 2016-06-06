df0a53ceeb8bf61339711aae6c62059c2caf6dcd
natdjerf <nadjerf@gmail.com>
2016-06-06 12:27:55 -0400

    Change format on date model
    Getting an error message on load for unable to transform date.
    Revising later

--- app/reservation/model.js
+++ app/reservation/model.js
@@ -1,11 +1,14 @@
-import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
+// import Model from 'ember-data/model';
+// import attr from 'ember-data/attr';
+import DS from 'ember-data';
 
 
-export default Model.extend({
-  permit_quantity : attr('number'),
-  lodging_quantity : attr('number'),
-  permit_start_date : attr('date'),
-  permit_end_date : attr('date'),
-  total_cost : attr('number')
+export default DS.Model.extend({
+  permit_quantity : DS.attr('number'),
+  lodging_quantity : DS.attr('number'),
+  permit_start_date : DS.attr('string', {
+    defaultValue() { return new Date(); }
+  }),
+  permit_end_date : DS.attr('date'),
+  total_cost : DS.attr('number')
 });
