4e9d9865268ebe07e629b318a083a332e5d7d5da
natdjerf <nadjerf@gmail.com>
2016-06-06 14:49:34 -0400

    DELETE action
    Add delete button
    Successfully delete Reservation from store
    Need to render new template/revert home on success

--- app/components/reservation-booking/reservation-view/component.js
+++ app/components/reservation-booking/reservation-view/component.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  form: {},
+  actions: {
+    submit(reservation) {
+      Ember.set(reservation,'reservation', this.get('form'));
+      this.sendAction('submit', this.get('form'));
+      console.log(reservation);
+    },
+    deleteReservation () {
+      this.get('reservation').destroyRecord();
+    }
+  },
 });
