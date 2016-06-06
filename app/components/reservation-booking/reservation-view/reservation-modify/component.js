db4867fb9114be9a72b58b0889b604dd8ef52163
natdjerf <nadjerf@gmail.com>
2016-06-06 16:19:35 -0400

    Additional component for modify
    Testing individual component & embedded component

---
+++ app/components/reservation-booking/reservation-view/reservation-modify/component.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  // Patch action here:
+  // form: {},
+  // actions: {
+  //   submit(reservation) {
+  //     Ember.set(reservation,'reservation', this.get('form'));
+  //     // this.sendAction('submit', this.get('form'));
+  //     console.log(reservation.id);
+  //   },
+  // },
+});
