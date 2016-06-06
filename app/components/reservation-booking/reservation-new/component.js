40e8278c7341e2e05b6955c320648c16d8eaa145
natdjerf <nadjerf@gmail.com>
2016-06-06 14:52:25 -0400

    Clean up CREATE request
    remove test functions

--- app/components/reservation-booking/reservation-new/component.js
+++ app/components/reservation-booking/reservation-new/component.js
@@ -4,14 +4,9 @@ export default Ember.Component.extend({
   form: {},
   actions: {
     submit(reservation) {
-      // Ember.set(reservation, 'reservation', this.get('user'));
-      // console.log(reservation);
-      // this.sendAction('submit', this.get('form'));
       Ember.set(reservation,'reservation', this.get('form'));
       this.sendAction('submit', this.get('form'));
       console.log(reservation);
     },
   },
 });
-
-// Set must be called with three or four arguments; an object, a property key, a value and tolerant true/false
