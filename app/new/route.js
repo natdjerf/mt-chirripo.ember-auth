65331ee156cc74d3c262f59f8f8ab0f048a550ae
natdjerf <nadjerf@gmail.com>
2016-06-06 12:27:05 -0400

    Route for new reservation
    CREATE action template

--- app/new/route.js
+++ app/new/route.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     createReservation (data) {
+      console.log(data);
       let reservation = this.get('store').createRecord('reservation', data);
       return reservation.save();
     }
