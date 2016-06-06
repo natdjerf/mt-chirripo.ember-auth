31b9ebb900e0ff4d2aee48be7baa03d53dfb1cc1
natdjerf <nadjerf@gmail.com>
2016-06-06 12:26:00 -0400

    Test out different date models
    Console logging for data testing

--- app/components/reservation-booking/component.js
+++ app/components/reservation-booking/component.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     createReservation (reservation) {
+      console.log(reservation);
       this.sendAction('createReservation', reservation);
     }
   },
