79523ef7c7621aa456bccfd677368044e238281c
natdjerf <nadjerf@gmail.com>
2016-06-07 10:43:09 -0400

    View details text content-editable
    Capturing changes in component

--- app/components/reservation-booking/reservation-view/component.js
+++ app/components/reservation-booking/reservation-view/component.js
@@ -1,15 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  form: {},
   actions: {
-    submit(reservation) {
-      Ember.set(reservation,'reservation', this.get('form'));
-      this.sendAction('submit', this.get('form'));
-      console.log(reservation);
-    },
     deleteReservation () {
       this.get('reservation').destroyRecord();
+    },
+    modifyReservation () {
+      let reservation = {};
+      reservation.permit_quantity = this.$('span.permit_quantity').text();
+      reservation.lodging_quantity = this.$('span.lodging_quantity').text();
+      reservation.permit_start_ = this.$('span.permit_quantity').text();
+      reservation.permit_quantity = this.$('span.permit_quantity').text();
+      reservation.total_cost = this.$('span.total_cost').text();
+      console.log(reservation);
     }
   },
 });
