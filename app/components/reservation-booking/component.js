4160ddf0f21a507476cc71d6b0df051efb9cca69
natdjerf <nadjerf@gmail.com>
2016-06-07 15:32:13 -0400

    Remove actions.
    Don't pass through this component.

--- app/components/reservation-booking/component.js
+++ app/components/reservation-booking/component.js
@@ -1,10 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    createReservation (reservation) {
-      console.log(reservation);
-      this.sendAction('createReservation', reservation);
-    }
-  },
 });
