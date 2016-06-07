83bb93b8f2a56b1a66f39a69b2353f6ffc65db3e
natdjerf <nadjerf@gmail.com>
2016-06-07 11:39:08 -0400

    Successfully passing data to route
    Send reservation data, relocate DELETE action to route
    Send reservation data, relocate PATCH action to route

--- app/components/reservation-booking/reservation-view/component.js
+++ app/components/reservation-booking/reservation-view/component.js
@@ -3,16 +3,19 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     deleteReservation () {
-      this.get('reservation').destroyRecord();
+      let reservation = this.get('reservation');
+      console.log('delete reservation', reservation);
+      this.sendAction('deleteReservation', reservation);
     },
     modifyReservation () {
       let reservation = {};
       reservation.permit_quantity = this.$('span.permit_quantity').text();
       reservation.lodging_quantity = this.$('span.lodging_quantity').text();
-      reservation.permit_start_ = this.$('span.permit_quantity').text();
+      reservation.permit_start_ = this.$('span.permit_start_date').text();
       reservation.permit_quantity = this.$('span.permit_quantity').text();
       reservation.total_cost = this.$('span.total_cost').text();
-      console.log(reservation);
+      console.log('modify reservation', reservation);
+      this.sendAction('modifyReservation', reservation);
     }
   },
 });
