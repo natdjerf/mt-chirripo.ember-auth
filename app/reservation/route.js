539051a8f0b8222a163acf6e4741ff81a9305e22
natdjerf <nadjerf@gmail.com>
2016-06-07 11:40:42 -0400

    PATCH & DELETE actions
    Actions (console logs) moved to route
    Passing both actions in template

--- app/reservation/route.js
+++ app/reservation/route.js
@@ -1,7 +1,21 @@
 import Ember from 'ember';
 
+
 export default Ember.Route.extend({
   model (params) {
     return this.get('store').findRecord('reservation', params.id);
   },
+  actions : {
+    deleteReservation(data) {
+      console.log('reservation route', data);
+    },
+    modifyReservation (data) {
+      console.log('reservation route', data);
+      // let id = this.get('reserver.id');
+      // let store = this.get('store');
+      //
+      // let reservation = this.get('store').createRecord('reservation', data);
+      // return reservation.save();
+    }
+  }
 });
