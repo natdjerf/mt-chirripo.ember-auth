695e17d82efd8b0c37a500927ce482b9aca8d6b8
natdjerf <nadjerf@gmail.com>
2016-06-07 11:59:24 -0400

    Testing routes
    Specified console log

--- app/components/reservation-booking/reservation-new/component.js
+++ app/components/reservation-booking/reservation-new/component.js
@@ -2,11 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   form: {},
+
   actions: {
     submit(reservation) {
       Ember.set(reservation,'reservation', this.get('form'));
       this.sendAction('submit', this.get('form'));
-      console.log(reservation);
+      console.log('New reservation:', reservation);
     },
   },
 });
