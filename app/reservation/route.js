a773c8dede63ea5fa7e9bf3022f386eb85227dfc
natdjerf <nadjerf@gmail.com>
2016-06-07 14:58:15 -0400

    Successful PATCH
    Updated reservation permit quantity.
    Console.log 'this' to identify where ID was.

--- app/reservation/route.js
+++ app/reservation/route.js
@@ -2,21 +2,35 @@ import Ember from 'ember';
 
 
 export default Ember.Route.extend({
+  flashMessages: Ember.inject.service(),
+
   model (params) {
     return this.get('store').findRecord('reservation', params.id);
   },
+
   actions : {
     deleteReservation(data) {
       console.log('reservation route', data);
       data.destroyRecord();
     },
     modifyReservation (data) {
-      console.log('reservation route', data);
-      // let id = this.get('reserver.id');
-      // let store = this.get('store');
-      //
-      // let reservation = this.get('store').createRecord('reservation', data);
-      // return reservation.save();
+      console.log(this);
+      let id = this.context.id;
+      let store = this.get('store');
+       store.findRecord('reservation', id)
+      .then((reservation) => {
+        reservation.set = ('reservation.permit_quantity', data.permit_quantity);
+        return reservation;
+      })
+      .then((reservation) => reservation.save())
+      .then(() => {
+        this.get('flashMessages')
+        .success('Your reservation has been updated.');
+      })
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('There seems to be a problem with your request. Please try again');
+        });
     }
   }
 });
