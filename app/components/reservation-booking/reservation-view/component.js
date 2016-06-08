d6a428b68f857d177c4c8a398af221096e62ee1f
natdjerf <nadjerf@gmail.com>
2016-06-07 21:03:57 -0400

    Testing edit fields

--- app/components/reservation-booking/reservation-view/component.js
+++ app/components/reservation-booking/reservation-view/component.js
@@ -14,7 +14,7 @@ export default Ember.Component.extend({
       reservation.lodging_quantity = this.$('span.lodging_quantity').text();
       reservation.permit_start_ = this.$('span.permit_start_date').text();
       reservation.permit_quantity = this.$('span.permit_quantity').text();
-      reservation.total_cost = this.$('span.total_cost').text();
+      // reservation.total_cost = this.$('span.total_cost').text();
       console.log('modify reservation', reservation);
       this.sendAction('modifyReservation', reservation);
     }
