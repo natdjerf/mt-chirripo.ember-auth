bc53d17b6b2321fd7b156e027ece9dd6a9198683
natdjerf <nadjerf@gmail.com>
2016-06-07 21:11:48 -0400

    Update console on new
    Install helper package

--- app/new/route.js
+++ app/new/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     createReservation (data) {
-      console.log(data);
+      console.log('create action', data);
       let reservation = this.get('store').createRecord('reservation', data);
       return reservation.save();
     }
