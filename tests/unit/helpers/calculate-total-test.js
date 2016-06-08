0270626de285c7011a5fefa4550968a880ca7861
natdjerf <nadjerf@gmail.com>
2016-06-07 21:13:08 -0400

    auto generated tests

---
+++ tests/unit/helpers/calculate-total-test.js
@@ -0,0 +1,10 @@
+import { calculateTotal } from 'mt-chirripo.ember-auth/helpers/calculate-total';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | calculate total');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = calculateTotal([42]);
+  assert.ok(result);
+});
