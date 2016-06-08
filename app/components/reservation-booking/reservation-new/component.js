33ffd045af5653a396806a1421541fff9fb138c3
natdjerf <nadjerf@gmail.com>
2016-06-08 16:46:00 -0400

    Successful POST
    With total cost!
    Updated reservation-new total cost computed property

--- app/components/reservation-booking/reservation-new/component.js
+++ app/components/reservation-booking/reservation-new/component.js
@@ -1,56 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  form: {},
+  reservation: {
+    totalCost: Ember.computed('permitQuantity', 'lodgingQuantity', 'permitStartDate', 'permitEndDate', function() {
+      let numPermits = Math.abs(Ember.get(this, 'permitQuantity'));
+      let numLodging = Math.abs(Ember.get(this, 'lodgingQuantity'));
+      let date1 = new Date(Ember.get(this, 'permitStartDate'));
+      let date2 = new Date(Ember.get(this, 'permitEndDate'));
 
-  computedTotal: Ember.computed('form.permit_quantity', 'form.lodging_quantity', 'form.permit_start_date', 'form.permit_end_date', function() {
-    let date1 = new Date(this.get('form.permit_start_date'));
-    let date2 = new Date(this.get('form.permit_end_date'));
-    let numPermits = Math.abs(this.get('form.permit_quantity'));
-    let numLodging = Math.abs(this.get('form.lodging_quantity'));
+      let timeDiff = Math.abs(date2.getTime() - date1.getTime());
+      let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
-    let timeDiff = Math.abs(date2.getTime() - date1.getTime());
-    let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+      let permitCost = (diffDays + 1) * numPermits * 15;
+      let lodgingCost = 0;
+      if (numLodging !== 0) {
+        lodgingCost = (diffDays * numLodging * 35);
+      }
 
-    let permitCost = (diffDays + 1) * numPermits * 15;
-    let lodgingCost = 0;
-    if (numLodging !== 0) {
-      lodgingCost = (diffDays * numLodging * 35);
-    }
+      let calcualedTotalCost = (permitCost + lodgingCost);
 
-    let calcualedTotalCost = Math.abs(permitCost + lodgingCost);
+      return calcualedTotalCost;
+    }),
+  },
 
-    return calcualedTotalCost;
-  }),
+
 
   actions: {
     submit(reservation) {
-      Ember.set(reservation,'reservation', this.get('form'));
-      // .then((reservation) => {
-      // reservation.total_cost.set = ('reservation', this.get('test'));
-      // reservation.set = ('reservation.lodging_quantity', data.permit_quantity);
-      // reservation.set = ('reservation.permit_quantity', data.permit_quantity);
-      // reservation.set = ('reservation.permit_quantity', data.permit_quantity);
-      // reservation.set = ('reservation.permit_quantity', data.permit_quantity);
-      //   return reservation;
-      // })
-      // .then((reservation) => reservation.save())
-      // .then(() => {
-      //   this.get('flashMessages')
-      //   .success('Your reservation has been updated.');
-      // })
-      // .catch(() => {
-      //   this.get('flashMessages')
-      //   .danger('There seems to be a problem with your request. Please try again');
-      //   });
-      // console.log(this.testComputed);
-      // console.log(this.get('testComputed'));
-      reservation.totalCost = (this.get('computedTotal'));
+      Ember.set(reservation, 'totalCost', this.get('reservation.totalCost'));
+      console.log(this.get('reservation.totalCost'));
       this.sendAction('submit', reservation);
       console.log('New reservation:', reservation);
-
-
-
     },
   },
 });
--- app/new/route.js
+++ app/new/route.js
@@ -2,17 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
-    createReservation (data) {
+    createReservation(data) {
       console.log('create action', data);
       let reservation = this.get('store').createRecord('reservation', data);
       return reservation.save()
-      .then(() => {
-        this.get('flashMessages')
-        .success('Your reservation has been saved');
-      })
-      .catch(() => {
-        this.get('flashMessages')
-        .danger('There seems to be a problem with your request. Please try again');
+        .then(() => {
+          this.get('flashMessages')
+            .success('Your reservation has been saved');
+        })
+        .catch(() => {
+          this.get('flashMessages')
+            .danger('There seems to be a problem with your request. Please try again');
         });
     }
   }
