99330dc54f924718ad3dd762f1520e1b7c176cda
natdjerf <nadjerf@gmail.com>
2016-06-09 19:13:54 -0400

    Update styling
    Set up landing page, move text to about
    Add details to planning

--- app/index/route.js
+++ app/index/route.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  activate: function(){
+      $('body').css('background', "url('https://c4.staticflickr.com/1/685/22639053571_7680688325_k.jpg') no-repeat center center scroll");
+      $('body').css('background-attachment', 'fixed');
+      $('body').css('background-size', 'cover');
+      Ember.$('body').append();
+    },
+deactivate: function(){
+      $('body').css('background-image', "none");
+    }
 });
