b2f6c68d9c48cb8f602b575972d53423eab6902a
natdjerf <nadjerf@gmail.com>
2016-06-17 11:01:40 -0400

    Update UI
    Templates transparent, centered
    Change to sign up on sign in
    Remove index CSS
    Update nav link colors

--- app/about/route.js
+++ app/about/route.js
@@ -1,8 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  auth: Ember.inject.service(),
-
-  user: Ember.computed.alias('auth.credentials.email'),
-  isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
 });
--- app/index/route.js
+++ app/index/route.js
@@ -5,12 +5,10 @@ export default Ember.Route.extend({
     Ember.$('body').css('background', "url('/images/IMG_4941.JPG') no-repeat center center scroll");
     Ember.$('body').css('background-attachment', 'fixed');
     Ember.$('body').css('background-size', 'cover');
-    Ember.$('.navbar-default .navbar-nav > li > a').css('color', 'white');
     Ember.$('body').append();
   },
   deactivate: function() {
     Ember.$('body').css('background-image', "none");
-    Ember.$('.navbar-default .navbar-nav > li > a').css('color', '#12772A');
   },
 
 });
